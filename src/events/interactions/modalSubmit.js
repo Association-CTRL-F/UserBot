043e52737620e96c6ce1a3afb96740a87dfd35eb
Asthox <thomas.legoff0206@orange.fr>
2022-05-04 09:29:19 +0200

    Correction interaction

--- src/events/interactions/modalSubmit.js
+++ src/events/interactions/modalSubmit.js
@@ -2,8 +2,9 @@ export default (interaction, client) => {
 	if (interaction.type === 'MODAL_SUBMIT') {
 		const modal = client.modals.get(interaction.customId)
 		if (!modal)
-			return modal.reply({
-				content: `Impossible de trouver la modal "${modal.customId}"`,
+			return interaction.reply({
+				content: `Impossible de trouver la modal "${interaction.customId}"`,
+				ephemeral: true,
 			})
 
 		return modal.interaction(interaction, client)
