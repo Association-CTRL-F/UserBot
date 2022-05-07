ee72acea7a2139924335a29431caeca8ea13213e
Asthox <thomas.legoff0206@orange.fr>
2022-05-07 21:37:38 +0200

    Commande /commands dédoublée en /commands-admin pour la gestion des permissions

---
+++ src/slash-commands/commands/command-admin.js
@@ -0,0 +1,154 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable default-case */
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { db } from '../../util/util.js'
+import { Modal, TextInputComponent, showModal } from 'discord-modals'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('commands-admin')
+		.setDescription('Gère les commandes personnalisées')
+		.addSubcommand(subcommand =>
+			subcommand.setName('create').setDescription('Crée une nouvelle commande'),
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('edit')
+				.setDescription('Modifie une commande')
+				.addStringOption(option =>
+					option
+						.setName('nom')
+						.setDescription('Nom de la commande à modifier')
+						.setRequired(true),
+				),
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('delete')
+				.setDescription('Supprime une commande')
+				.addStringOption(option =>
+					option.setName('nom').setDescription('Nom de la commande').setRequired(true),
+				),
+		),
+	interaction: async (interaction, client) => {
+		// Acquisition du nom
+		const nom = interaction.options.getString('nom')
+
+		// Acquisition de la base de données
+		const bdd = await db(client, 'userbot')
+		if (!bdd)
+			return interaction.reply({
+				content: 'Une erreur est survenue lors de la connexion à la base de données 😕',
+				ephemeral: true,
+			})
+
+		// Vérification si la commande existe
+		let commandBdd = {}
+		try {
+			const sqlCheckName = 'SELECT * FROM commands WHERE name = ?'
+			const dataCheckName = [nom]
+			const [resultCheckName] = await bdd.execute(sqlCheckName, dataCheckName)
+			commandBdd = resultCheckName[0]
+		} catch (error) {
+			return interaction.reply({
+				content:
+					'Une erreur est survenue lors de la récupération de la commande en base de données 😕',
+				ephemeral: true,
+			})
+		}
+
+		switch (interaction.options.getSubcommand()) {
+			// Nouvelle commande
+			case 'create':
+				const modalCreate = new Modal()
+					.setCustomId('command-create')
+					.setTitle("Création d'une nouvelle commande")
+					.addComponents(
+						new TextInputComponent()
+							.setCustomId('name-command-create')
+							.setLabel('Nom de la commande')
+							.setStyle('SHORT')
+							.setMinLength(1)
+							.setMaxLength(255)
+							.setRequired(true),
+					)
+					.addComponents(
+						new TextInputComponent()
+							.setCustomId('content-command-create')
+							.setLabel('Contenu de la commande')
+							.setStyle('LONG')
+							.setMinLength(1)
+							.setRequired(true),
+					)
+
+				return showModal(modalCreate, {
+					client: client,
+					interaction: interaction,
+				})
+
+			// Modifie une commande
+			case 'edit':
+				// Vérification que la commande existe bien
+				if (!commandBdd)
+					return interaction.reply({
+						content: `La commande **${nom}** n'existe pas 😕`,
+						ephemeral: true,
+					})
+
+				const modalEdit = new Modal()
+					.setCustomId('command-edit')
+					.setTitle("Modification d'une commande")
+					.addComponents(
+						new TextInputComponent()
+							.setCustomId('name-command-edit')
+							.setLabel('Nom de la commande')
+							.setStyle('SHORT')
+							.setDefaultValue(commandBdd.name)
+							.setMinLength(1)
+							.setMaxLength(255)
+							.setRequired(true),
+					)
+					.addComponents(
+						new TextInputComponent()
+							.setCustomId('content-command-edit')
+							.setLabel('Nouveau contenu de la commande')
+							.setStyle('LONG')
+							.setDefaultValue(commandBdd.content)
+							.setMinLength(1)
+							.setRequired(true),
+					)
+
+				return showModal(modalEdit, {
+					client: client,
+					interaction: interaction,
+				})
+
+			// Supprime une commande
+			case 'delete':
+				// Vérification que la commande existe bien
+				if (!commandBdd)
+					return interaction.reply({
+						content: `La commande **${nom}** n'existe pas 😕`,
+						ephemeral: true,
+					})
+
+				// Si oui, alors suppression de la commande en base de données
+				try {
+					const sqlDelete = 'DELETE FROM commands WHERE name = ?'
+					const dataDelete = [nom]
+
+					await bdd.execute(sqlDelete, dataDelete)
+				} catch {
+					return interaction.reply({
+						content:
+							'Une erreur est survenue lors de la suppression de la commande en base de données 😬',
+						ephemeral: true,
+					})
+				}
+
+				return interaction.reply({
+					content: `La commande **${nom}** a bien été supprimée 👌`,
+				})
+		}
+	},
+}
--- src/slash-commands/commands/commands.js
+++ src/slash-commands/commands/commands.js
@@ -3,12 +3,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { db, convertDateForDiscord } from '../../util/util.js'
 import { Pagination } from 'pagination.djs'
-import { Modal, TextInputComponent, showModal } from 'discord-modals'
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('command')
-		.setDescription('Gère les commandes personnalisées')
+		.setName('commands')
+		.setDescription('Commandes personnalisées')
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('view')
@@ -27,33 +26,8 @@ export default {
 						.setDescription('Mot clé de la recherche (par nom)')
 						.setRequired(true),
 				),
-		)
-		.addSubcommand(subcommand =>
-			subcommand.setName('create').setDescription('Crée une nouvelle commande'),
-		)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('edit')
-				.setDescription('Modifie une commande')
-				.addStringOption(option =>
-					option
-						.setName('nom')
-						.setDescription('Nom de la commande à modifier')
-						.setRequired(true),
-				),
-		)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('delete')
-				.setDescription('Supprime une commande')
-				.addStringOption(option =>
-					option.setName('nom').setDescription('Nom de la commande').setRequired(true),
-				),
 		),
 	interaction: async (interaction, client) => {
-		// Acquisition du nom
-		const nom = interaction.options.getString('nom')
-
 		// Acquisition de la base de données
 		const bdd = await db(client, 'userbot')
 		if (!bdd)
@@ -62,24 +36,27 @@ export default {
 				ephemeral: true,
 			})
 
-		// Vérification si la commande existe
-		let commandBdd = {}
-		try {
-			const sqlCheckName = 'SELECT * FROM commands WHERE name = ?'
-			const dataCheckName = [nom]
-			const [resultCheckName] = await bdd.execute(sqlCheckName, dataCheckName)
-			commandBdd = resultCheckName[0]
-		} catch (error) {
-			return interaction.reply({
-				content:
-					'Une erreur est survenue lors de la récupération de la commande en base de données 😕',
-				ephemeral: true,
-			})
-		}
-
 		switch (interaction.options.getSubcommand()) {
 			// Visualisation des commandes
 			case 'view':
+				// Acquisition du nom
+				const nom = interaction.options.getString('nom')
+
+				// Vérification si la commande existe
+				let commandBdd = {}
+				try {
+					const sqlCheckName = 'SELECT * FROM commands WHERE name = ?'
+					const dataCheckName = [nom]
+					const [resultCheckName] = await bdd.execute(sqlCheckName, dataCheckName)
+					commandBdd = resultCheckName[0]
+				} catch (error) {
+					return interaction.reply({
+						content:
+							'Une erreur est survenue lors de la récupération de la commande en base de données 😕',
+						ephemeral: true,
+					})
+				}
+
 				if (nom) {
 					// Vérification que la commande existe bien
 					if (!commandBdd)
@@ -301,98 +278,6 @@ export default {
 
 				// Envoi de l'embed
 				return paginationSearch.render()
-
-			// Nouvelle commande
-			case 'create':
-				const modalCreate = new Modal()
-					.setCustomId('command-create')
-					.setTitle("Création d'une nouvelle commande")
-					.addComponents(
-						new TextInputComponent()
-							.setCustomId('name-command-create')
-							.setLabel('Nom de la commande')
-							.setStyle('SHORT')
-							.setMinLength(1)
-							.setMaxLength(255)
-							.setRequired(true),
-					)
-					.addComponents(
-						new TextInputComponent()
-							.setCustomId('content-command-create')
-							.setLabel('Contenu de la commande')
-							.setStyle('LONG')
-							.setMinLength(1)
-							.setRequired(true),
-					)
-
-				return showModal(modalCreate, {
-					client: client,
-					interaction: interaction,
-				})
-
-			// Modifie une commande
-			case 'edit':
-				// Vérification que la commande existe bien
-				if (!commandBdd)
-					return interaction.reply({
-						content: `La commande **${nom}** n'existe pas 😕`,
-						ephemeral: true,
-					})
-
-				const modalEdit = new Modal()
-					.setCustomId('command-edit')
-					.setTitle("Modification d'une commande")
-					.addComponents(
-						new TextInputComponent()
-							.setCustomId('name-command-edit')
-							.setLabel('Nom de la commande')
-							.setStyle('SHORT')
-							.setDefaultValue(commandBdd.name)
-							.setMinLength(1)
-							.setMaxLength(255)
-							.setRequired(true),
-					)
-					.addComponents(
-						new TextInputComponent()
-							.setCustomId('content-command-edit')
-							.setLabel('Nouveau contenu de la commande')
-							.setStyle('LONG')
-							.setDefaultValue(commandBdd.content)
-							.setMinLength(1)
-							.setRequired(true),
-					)
-
-				return showModal(modalEdit, {
-					client: client,
-					interaction: interaction,
-				})
-
-			// Supprime une commande
-			case 'delete':
-				// Vérification que la commande existe bien
-				if (!commandBdd)
-					return interaction.reply({
-						content: `La commande **${nom}** n'existe pas 😕`,
-						ephemeral: true,
-					})
-
-				// Si oui, alors suppression de la commande en base de données
-				try {
-					const sqlDelete = 'DELETE FROM commands WHERE name = ?'
-					const dataDelete = [nom]
-
-					await bdd.execute(sqlDelete, dataDelete)
-				} catch {
-					return interaction.reply({
-						content:
-							'Une erreur est survenue lors de la suppression de la commande en base de données 😬',
-						ephemeral: true,
-					})
-				}
-
-				return interaction.reply({
-					content: `La commande **${nom}** a bien été supprimée 👌`,
-				})
 		}
 	},
 }
