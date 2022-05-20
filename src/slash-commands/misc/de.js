197c8e4979d69ba0219ed68566a6176f6a46d62b
Asthox <thomas.legoff0206@orange.fr>
2022-05-20 18:26:19 +0200

    Ajout commande /dé

---
+++ src/slash-commands/misc/de.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+export default {
+	data: new SlashCommandBuilder().setName('dé').setDescription('Lancer de dé'),
+	interaction: interaction => {
+		const random = Math.floor(Math.random() * 6) + 1
+
+		// Création de l'embed
+		const embed = {
+			color: '#C27C0E',
+			title: 'Lancer de dé',
+			description: `Tu es tombé sur **${random}**`,
+			thumbnail: {
+				url: `attachment://${random}.png`,
+			},
+		}
+
+		return interaction.reply({ embeds: [embed], files: [`./assets/commands/dé/${random}.png`] })
+	},
+}
