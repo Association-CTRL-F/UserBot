9f08bb133d3359a6b1d2d1a7ac784c81c3437a41
Asthox <thomas.legoff0206@orange.fr>
2022-05-22 20:27:00 +0200

    Amélioration /dé

--- src/slash-commands/misc/de.js
+++ src/slash-commands/misc/de.js
@@ -3,18 +3,43 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 export default {
 	data: new SlashCommandBuilder().setName('dé').setDescription('Lancer de dé'),
 	interaction: interaction => {
-		const random = Math.floor(Math.random() * 6) + 1
+		// Lancement du dé
+		const face = Math.floor(Math.random() * 6) + 1
+
+		// Surprise
+		const randomSurprise = Math.round(Math.random() * 100)
+		const randomTirage = Math.round(Math.random() * 100)
+
+		if (randomSurprise === randomTirage) {
+			// Création de l'embed surprise
+			const embed = {
+				color: '#1ABC9C',
+				title: 'Lancer de dé',
+				description: `**SURPRISE**`,
+				thumbnail: {
+					url: `attachment://rgb.png`,
+				},
+			}
+
+			return interaction.reply({
+				embeds: [embed],
+				files: [`./config/commands/dé/rgb.png`],
+			})
+		}
 
 		// Création de l'embed
 		const embed = {
 			color: '#C27C0E',
 			title: 'Lancer de dé',
-			description: `Tu es tombé sur **${random}**`,
+			description: `Tu es tombé sur **${face}**`,
 			thumbnail: {
-				url: `attachment://${random}.png`,
+				url: `attachment://${face}.png`,
 			},
 		}
 
-		return interaction.reply({ embeds: [embed], files: [`./config/commands/dé/${random}.png`] })
+		return interaction.reply({
+			embeds: [embed],
+			files: [`./config/commands/dé/${face}.png`],
+		})
 	},
 }
