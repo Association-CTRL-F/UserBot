2f4002d9d120677c1ccb1c1fb958a36d38f1060e
Asthox <thomas.legoff0206@orange.fr>
2022-05-22 20:27:12 +0200

    Regroupement de la commande /infos

---
+++ src/slash-commands/misc/infos.js
@@ -0,0 +1,131 @@
+/* eslint-disable default-case */
+/* eslint-disable no-case-declarations */
+import { convertDateForDiscord, diffDate } from '../../util/util.js'
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+// import nodePackage from '../../../package.json'
+import { readFileSync } from 'fs'
+const { version } = JSON.parse(readFileSync('./package.json'))
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('infos')
+		.setDescription('Donne quelques infos sur le serveur et le bot')
+		.addSubcommand(subcommand => subcommand.setName('bot').setDescription('Infos du bot'))
+		.addSubcommand(subcommand =>
+			subcommand.setName('server').setDescription('Infos du serveur'),
+		),
+	interaction: (interaction, client) => {
+		switch (interaction.options.getSubcommand()) {
+			case 'bot':
+				const embedBot = {
+					color: '#3366FF',
+					author: {
+						name: `${client.user.username} (ID ${client.user.id})`,
+						icon_url: client.user.displayAvatarURL({ dynamic: true }),
+					},
+					fields: [
+						{
+							name: 'Latence API',
+							value: `${client.ws.ping} ms`,
+						},
+						{
+							name: 'Uptime',
+							value: diffDate(client.readyAt),
+						},
+						{
+							name: 'Préfixe',
+							value: `\`${client.config.bot.prefix}\``,
+						},
+						{
+							name: 'Version',
+							value: version,
+						},
+					],
+				}
+
+				return interaction.reply({ embeds: [embedBot] })
+
+			case 'server':
+				const premiumTier = {
+					NONE: 'Non spécifié',
+					TIER_1: 'Niveau 1',
+					TIER_2: 'Niveau 2',
+					TIER_3: 'Niveau 3',
+				}
+
+				const mfaLevel = { NONE: 'Désactivé', ELEVATED: 'Activé' }
+
+				const verificationLevel = {
+					NONE: 'Non spécifié',
+					LOW: 'Faible : email vérifié requis.',
+					MEDIUM: 'Moyen : sur Discord depuis 5 minutes.',
+					HIGH: 'Élevé : sur le serveur depuis 10 minutes.',
+					VERY_HIGH: 'Très élevé : numéro de téléphone vérifié.',
+				}
+
+				const embedServer = {
+					color: '#3366FF',
+					author: {
+						name: `${interaction.guild.name} (ID ${interaction.guild.id})`,
+						icon_url: interaction.guild.iconURL({ dynamic: true }),
+						url: interaction.guild.vanityURL,
+					},
+					fields: [
+						{
+							name: '**Date de création**',
+							value: convertDateForDiscord(interaction.guild.createdAt),
+							inline: true,
+						},
+						{
+							name: '**Âge du serveur**',
+							value: `${diffDate(interaction.guild.createdAt)}`,
+							inline: true,
+						},
+						{
+							name: '**Propriétaire du serveur**',
+							value: `<@${interaction.guild.ownerId}>`,
+							inline: true,
+						},
+						{
+							name: '**Rôle le plus élevé**',
+							value: `${interaction.guild.roles.highest}`,
+							inline: true,
+						},
+						{
+							name: '**Nombre de membres**',
+							value: `${interaction.guild.memberCount}/${interaction.guild.maximumMembers}`,
+							inline: true,
+						},
+						{
+							name: "**Nombre d'émojis**",
+							value: `${interaction.guild.emojis.cache.size}`,
+							inline: true,
+						},
+						{
+							name: '**Nombre de canaux**',
+							value: `${interaction.guild.channels.cache.size}`,
+							inline: true,
+						},
+						{
+							name: '**Niveau de vérification**',
+							value: `${verificationLevel[interaction.guild.verificationLevel]}`,
+							inline: true,
+						},
+						{
+							name: '**A2F**',
+							value: `${mfaLevel[interaction.guild.mfaLevel]}`,
+							inline: true,
+						},
+						{
+							name: '**Niveau Boost Nitro**',
+							value: `${premiumTier[interaction.guild.premiumTier]}`,
+							inline: true,
+						},
+					],
+				}
+
+				return interaction.reply({ embeds: [embedServer] })
+		}
+	},
+}
