7c8b97dbcc8675b8dc45177fead39500ffc64d0e
Asthox <thomas.legoff0206@orange.fr>
2022-09-30 01:18:39 +0200

    Fix commands handler

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -482,9 +482,11 @@ export default async (message, client) => {
 	// Command handler
 	if (message.content.startsWith(configGuild.COMMANDS_PREFIX)) {
 		const regexCommands = `${configGuild.COMMANDS_PREFIX}([a-zA-Z0-9+]+)(?: .*|$)`
-		const args = message.content.match(regexCommands)
-		const commandName = args[1].toLowerCase()
 
+		const args = message.content.match(regexCommands)
+		if (!args) return
+
+		const commandName = args[1].toLowerCase()
 		if (!commandName) return
 
 		// Vérification si la commande existe
