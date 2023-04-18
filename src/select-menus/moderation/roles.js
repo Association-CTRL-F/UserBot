f5faa810af1edf396f5ad3a01f5eaf95b08123be
Asthox <thomas.legoff0206@orange.fr>
2023-04-19 01:10:28 +0200

    feat: add LOGS_ROLES_CHANNEL_ID + ajustements

--- src/select-menus/moderation/roles.js
+++ src/select-menus/moderation/roles.js
@@ -1,3 +1,6 @@
+import { EmbedBuilder } from 'discord.js'
+import { displayNameAndID } from '../../util/util.js'
+
 export default {
 	data: {
 		name: 'roles',
@@ -15,13 +18,40 @@ export default {
 		// Acquisition du membre
 		const member = menu.guild.members.cache.get(menu.user.id)
 
+		// Acquisition du salon de logs
+		const rolesLogs = menu.guild.channels.cache.get(
+			client.config.guild.channels.LOGS_ROLES_CHANNEL_ID,
+		)
+
+		// Vérification si double rôle
+		if (menu.values.includes(modo.id) && menu.values.includes(certifie.id))
+			return menu.reply({
+				content: 'Tu ne peux pas avoir les rôles Modo et Certifiés en même temps 😬',
+				ephemeral: true,
+			})
+
 		await rolesArray.forEach(role => {
 			if (member.roles.cache.has(role)) member.roles.remove(role)
 		})
 
+		let description = ''
 		await menu.values.forEach(role => {
 			member.roles.add(role)
+			description = description.concat(`- <@&${role}>\n`)
 		})
+
+		// Envoi du message de logs
+		const embed = new EmbedBuilder()
+			.setColor('00FF00')
+			.setTitle('Rôles modifiés')
+			.setDescription(description)
+			.setAuthor({
+				name: displayNameAndID(menu.member),
+				iconURL: menu.user.displayAvatarURL({ dynamic: true }),
+			})
+			.setTimestamp()
+
+		await rolesLogs.send({ embeds: [embed] })
 
 		return menu.reply({ content: 'Les rôles ont bien été mis à jour 👌', ephemeral: true })
 	},
