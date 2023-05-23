ab1748263c8780a9436fa25a39c7cd78db184351
Asthox <thomas.legoff0206@orange.fr>
2023-05-23 18:58:38 +0200

    fix: free games

--- src/events/client/freeGamesLoader.js
+++ src/events/client/freeGamesLoader.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-await-in-loop */
 /* eslint-disable id-length */
-/* eslint-disable no-undefined */
+
 import { readFile, writeFile } from 'fs/promises'
 import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js'
 import axios from 'axios'
@@ -20,78 +22,90 @@ export default (guild, client) => {
 		)
 
 		// Récupérer les données de l'API
-		axios
-			.get(
-				'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr-FR&country=FR&allowCountries=FR',
-			)
-			.then(res => {
-				// regarder si un jeu n'est plus gratuit
-				Object.values(freeGamesJSON).forEach(async game => {
-					const gameFound = res.data.data.Catalog.searchStore.elements.find(
-						g => g.id === game.id,
-					)
-					if (gameFound === undefined) {
-						// Si le jeu n'est pas présent dans la liste
-						// des jeux gratuits on le supprime
-						delete freeGamesJSON[game.id]
-						// On prépare un embed
-						const embed = new EmbedBuilder()
-							.setTitle(
-								`Le jeu ${game.name} n'est plus gratuit sur le Store Epic Games !`,
-							)
-							.setColor('C9572A')
+		try {
+			axios
+				.get(
+					'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr-FR&country=FR&allowCountries=FR',
+				)
+				.then(async res => {
+					// regarder si un jeu n'est plus gratuit
+					for (let i = 0; i < freeGamesJSON.length; i++) {
+						const game = freeGamesJSON[i]
+						let gameFound = false
+						for (let j = 0; j < res.data.data.Catalog.searchStore.elements.length; j++)
+							if (game.id === res.data.data.Catalog.searchStore.elements[j].id) {
+								gameFound = true
+								break
+							}
 
-						// On envoie le message
-						await freeGamesChannel.send({ embeds: [embed] })
-					}
-				})
+						if (!gameFound) {
+							freeGamesJSON.splice(i, 1)
+							// On prépare un embed
+							const embed = new EmbedBuilder()
+								.setTitle(
+									`Le jeu ${game.title} n'est plus gratuit sur le Store Epic Games !`,
+								)
+								.setColor('C9572A')
 
-				// regarder si le jeu se trouve dans le fichier freeGames.json
-				res.data.data.Catalog.searchStore.elements.forEach(async game => {
-					// if game name = Mystery Game, ignore
-					if (game.title === 'Mystery Game') return
-					const gameFound = Object.values(freeGamesJSON).find(g => g.id === game.id)
-					if (gameFound === undefined) {
-						// Si le jeu n'est pas présent
-						// dans le fichier on l'ajoute
-						freeGamesJSON[game.id] = {
-							id: game.id,
-							name: game.title,
-							url: game.productSlug,
-							image: game.keyImages[0].url,
+							// On envoie le message
+							await freeGamesChannel.send({ embeds: [embed] })
 						}
-						const row = new ActionRowBuilder().addComponents(
-							new ButtonBuilder()
-								.setLabel('Acheter')
-								.setURL(`https://www.epicgames.com/store/fr/p/${game.productSlug}`)
-								.setStyle(ButtonStyle.Link),
-						)
+					}
 
-						// On prépare un embed
-						const embed = new EmbedBuilder()
-							.setTitle(`Nouveau jeu gratuit sur le Store Epic Games !`)
-							.setColor('57C92A')
-							.setImage(game.keyImages[0].url)
-							.setDescription(
-								`Le jeu **${game.title}** est gratuit sur le Store Epic Games !\n\n> ${game.description}`,
+					for (let i = 0; i < res.data.data.Catalog.searchStore.elements.length; i++) {
+						const game = res.data.data.Catalog.searchStore.elements[i]
+						let gameFound = false
+						for (let j = 0; j < freeGamesJSON.length; j++)
+							if (game.id === freeGamesJSON[j].id) {
+								gameFound = true
+								break
+							}
+
+						if (!gameFound) {
+							if (game.title === 'Mystery Game') continue
+							freeGamesJSON.push({
+								id: game.id,
+								title: game.title,
+								description: game.description,
+								image: game.keyImages[0].url,
+								url: `https://www.epicgames.com/store/fr/p/${game.productSlug}`,
+							})
+
+							const row = new ActionRowBuilder().addComponents(
+								new ButtonBuilder()
+									.setLabel('Acheter')
+									.setURL(
+										`https://www.epicgames.com/store/fr/p/${game.productSlug}`,
+									)
+									.setStyle(ButtonStyle.Link),
 							)
 
-						// On envoie le message
-						await freeGamesChannel.send({
-							content: `<@&${freeGamesRole}>`,
-							embeds: [embed],
-							components: [row],
-						})
-					}
-				})
-			})
-			.catch(() => null)
+							// On prépare un embed
+							const embed = new EmbedBuilder()
+								.setTitle(`**${game.title}** est gratuit sur le Store Epic Games !`)
+								.setColor('57C92A')
+								.setImage(game.keyImages[0].url)
+								.setURL(`https://www.epicgames.com/store/fr/p/${game.productSlug}`)
+								.setDescription(`> ${game.description}`)
 
-		// On écrit dans le fichier freeGames.json les nouvelles valeurs
-		// après 5 secondes afin de s'assurer que les requêtes d'API
-		// aient le temps de s'effectuer
-		setTimeout(async () => {
-			await writeFile('./config/env/freeGames.json', JSON.stringify(freeGamesJSON, null, 2))
-		}, 5000)
+							// On envoie le message
+							await freeGamesChannel.send({
+								content: `<@&${freeGamesRole}>`,
+								embeds: [embed],
+								components: [row],
+							})
+
+							gameFound = false
+						}
+					}
+
+					await writeFile(
+						'./config/env/freeGames.json',
+						JSON.stringify(freeGamesJSON, null, 2),
+					)
+				})
+		} catch (err) {
+			console.log(`Erreur lors de la récupération des données de l'API : ${err}`)
+		}
 	}, 10000)
 }
