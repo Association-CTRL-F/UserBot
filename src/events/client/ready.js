f07e5b5bb033ee503301266eefea732a4fe0d316
Sonny <sonnyasdev@gmail.com>
2023-05-28 00:49:55 +0200

    temp fix: disable free games alert

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -5,7 +5,6 @@ import giveaways from './giveawaysLoader.js'
 import voiceChannels from './voiceChannelsLoader.js'
 import noSpeak from './noSpeakLoader.js'
 import gpus from './gpusLoader.js'
-import freeGames from './freeGamesLoader.js'
 import serverLoader from './serverLoader.js'
 
 export default async client => {
@@ -34,9 +33,6 @@ export default async client => {
 
 	// Alertes Nvidia
 	gpus(guild)
-
-	// Alertes jeux Epic Games Store
-	freeGames(guild, client)
 
 	// Lancement du serveur
 	serverLoader(client)
