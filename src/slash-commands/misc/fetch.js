76933ab2b684f5d87ab4de338710e6aef40d2992
Asthox <thomas.legoff0206@orange.fr>
2023-11-13 22:08:05 +0100

    fix

--- src/slash-commands/misc/fetch.js
+++ src/slash-commands/misc/fetch.js
@@ -1,13 +1,13 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
 
 export default {
-	data: new SlashCommandBuilder().setName('fetch').setDescription('Fetch'),
+	data: new SlashCommandBuilder().setName('fetchh').setDescription('Fetchh'),
 	interaction: async interaction => {
 		// Création de l'embed start
 		const logEmbedStart = new EmbedBuilder()
 			.setColor('57C92A')
 			.setDescription(
-				`**START** FETCH ALL GUILD MEMBERS : **${interaction.guild.memberCount} MEMBERS**\n\nACTION : pas de rôle et pending = false`,
+				`**START** FETCH ALL GUILD MEMBERS : **${interaction.guild.memberCount} MEMBERS**\n\nACTION : "Change ton pseudo"`,
 			)
 			.setTimestamp(new Date())
 
@@ -19,16 +19,12 @@ export default {
 		let userCount = 0
 		await interaction.guild.members.fetch().then(members =>
 			members.forEach(async member => {
-				if (
-					!member.user.bot &&
-					!member.roles.cache.has('475406297127452673') &&
-					!member.roles.cache.has('768209418319822868') &&
-					member.pending === false
-				) {
+				if (!member.user.bot && member.nickname === 'Change ton pseudo') {
 					userCount += 1
 					await interaction.channel.send({
-						content: `${member.user.username} (ID : ${member.user.id} ; Mention : <@${member.user.id}>)`,
+						content: `Modifié ✅ | ${member.user.username} (Nickname : ${member.nickname} ; ID : ${member.user.id} ; Mention : <@${member.user.id}>)`,
 					})
+					await member.setNickname(null, 'No nicknames allowed!')
 				}
 			}),
 		)
@@ -37,7 +33,7 @@ export default {
 		const logEmbedEnd = new EmbedBuilder()
 			.setColor('57C92A')
 			.setDescription(
-				`**END** FETCH ALL GUILD MEMBERS : **${interaction.guild.memberCount}**\n\nACTION : pas de rôle et pending = false\n\nMEMBERS TRIGGERED : **${userCount}**`,
+				`**END** FETCH ALL GUILD MEMBERS : **${interaction.guild.memberCount}**\n\nACTION : "Change ton pseudo"\n\nMEMBERS TRIGGERED : **${userCount}**`,
 			)
 			.setTimestamp(new Date())
 
